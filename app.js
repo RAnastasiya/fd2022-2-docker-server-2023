2bfb5daf3a33b34d04ab28864480e3d9ef3a7164
RAnastasiya <rizhko.anastasiya@gmail.com>
2023-05-15 13:54:25 +0300

    Add Dockerfile

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 
-app.use((req, res)=>{
-    res.send('Hello from container')
+app.get('/', (req, res)=>{
+    res.send('Hi!!!!')
 })
+
+app.get('/about', (req, res)=>{
+    res.send('Hello from About!!!!')
+})
+
 module.exports = app;
